6dd12d6a6b260cf9d939af477d299bd2b3490f7b
EmilsBlinds <78017398+EmilsBlinds@users.noreply.github.com>
2021-04-20 17:01:29 +0300

    shopping list

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+let list = []
+
+
+document.getElementById('addbutton').addEventListener('click', function(e){
+    e.preventDefault();
+
+    var li = {item: item.value}
+
+    console.log(item.value)
+})
+
