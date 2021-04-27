8a30f18663ab0af6745e04486feef383a3d65f6e
EmilsBlinds <78017398+EmilsBlinds@users.noreply.github.com>
2021-04-27 17:56:55 +0300

    todo

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,35 @@
-let list = []
+let list = [];
 
+window.addEventListener('load', () => {
+    list = JSON.parse(localStorage.getItem("list") || "[]");
+    console.log(list)
+    render();
+});
 
+let addbutton = document.getElementById('addbutton')
 document.getElementById('addbutton').addEventListener('click', function(e){
     e.preventDefault();
 
-    var li = {item: item.value}
+    let item = {item: item.value};
 
-    console.log(item.value)
-})
+    item.value = "";
 
+    list.push(item)
+
+    render()
+});
+
+function render() {
+    let saraksts = document.getElementById('saraksts');
+    saraksts.innerHTML = "";
+
+    for(let i = 0; i < list.length; i++) {
+        let item = `
+        <div class="gramata">
+            <h3>${list[i].item}</h3>
+        </div>`;
+
+        saraksts.innerHTML += item;
+    }
+    localStorage.setItem("list", JSON.stringify(list))
+}
